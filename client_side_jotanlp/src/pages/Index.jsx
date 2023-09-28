786148527fd0d4c82edd0dd7013a0b6fc64bc5a1
JR-Camacho <josecamachoc0303@gmail.com>
2023-09-28 13:46:56 -0400

    Add the new submodule

--- client_side_jotanlp/src/pages/Index.jsx
+++
@@ -1,5 +0,0 @@
-import Home from "./Home/Home";
-import PageNotFound from "./NotFound/PageNotFound";
-import SentimentAnalysis from "./SentimentAnalysis/Index"
-
-export { Home, PageNotFound, SentimentAnalysis };
